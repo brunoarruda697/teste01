e58ac3a182ffc2fe4ddd11e83fa40c6baa279215
Bruno Arruda Costa <brunoarruda697@gmail.com>
2018-05-12 14:26:20 -0300

    atualizando modificação

--- src/zero/um/teste/Teste01.java
+++ src/zero/um/teste/Teste01.java
@@ -2,6 +2,6 @@ package zero.um.teste;
 
 public class Teste01 {
 		public static void main(String[] args) {
-			System.out.println("testando comandos do git em casa");
+			System.out.println("modificando as coisas");
 		}
 }
